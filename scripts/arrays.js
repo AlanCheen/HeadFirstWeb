fa0b93e6d5c5696bb1b51b365ee86b81efe82902
zhening.czn <zhening.czn@alibaba-inc.com>
2021-03-05 16:38:30 +0800

    update

---
+++ scripts/arrays.js
@@ -0,0 +1,32 @@
+// 练习下数组
+
+
+var arr1 = [
+    {
+        "id":"1",
+        "name":"arr1",
+        "price":"arr111",
+        "boo":"boo1"
+    }, {
+        "id": "2",
+        "name": "arr2",
+        "price": "arr222",
+        "boo": "boo2"
+    },
+]
+
+var arr2 = [
+    {
+        "id": "1",
+        "name": "newName2",
+        "price": "arr111",
+    }, {
+        "id": "2",
+        "name": "newName3",
+        "price": "arr232323",
+        "boo": "boo2323"
+    },
+]
+
+
+
